c5d2b352a4467382a0ab3c2c7dd13c9770c222c4
terminalkitten <dk@amaniak.nl>
2026-05-05 14:31:45 +0200

    feat: add service for memory

--- django_ai_sdk/memories/__init__.py
+++ django_ai_sdk/memories/__init__.py
@@ -2,3 +2,6 @@
 
 # Import signals to register them when Django starts
 from django_ai_sdk.memories import signals  # noqa: F401
+from django_ai_sdk.memories.services import MemoryService
+
+__all__ = ["MemoryService"]
