f98e21dc52959a73ee5fd5fa6f43df6283a55770
terminalkitten <dk@amaniak.nl>
2026-05-20 22:33:49 +0200

    feat: add check for mime_types

--- django_ai_sdk/files/processors.py
+++ django_ai_sdk/files/processors.py
@@ -1,18 +1,38 @@
+from io import BytesIO
 from pathlib import Path
 from typing import Protocol, TypeVar
 
+import magic
+from django.core.files.base import File
+from django.core.files.uploadedfile import InMemoryUploadedFile, TemporaryUploadedFile
 from pydantic import BaseModel
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class FileProcessor(Protocol):
+    ALLOWED_MIME_TYPES: list[str] = []
+
+    def is_valid(
+        self, file: str | Path | BytesIO | InMemoryUploadedFile | TemporaryUploadedFile
+    ) -> bool:
+        """Check if file is valid"""
+        if isinstance(file, (str, Path)):
+            mime_type = magic.from_file(file, mime=True)
+        elif isinstance(file, TemporaryUploadedFile):
+            mime_type = magic.from_file(file.temporary_file_path(), mime=True)
+        else:
+            mime_type = magic.from_buffer(file.read(), mime=True)
+            file.seek(0)
+
+        return mime_type in self.ALLOWED_MIME_TYPES
+
     def run(
         self,
-        file: str | Path,
+        file: str | Path | File,
         *,
         response_format: type[T] | None = None,
-    ) -> T | str: ...
+    ) -> T | str | None: ...
 
 
 class ContentProcessor(Protocol):
@@ -21,4 +41,4 @@ class ContentProcessor(Protocol):
         content: str,
         *,
         response_format: type[T] | None = None,
-    ) -> T | str: ...
+    ) -> T | str | None: ...
