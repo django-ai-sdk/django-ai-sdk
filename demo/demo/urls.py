384ae0833bd0fd5838e44512c68424994b8ba5c7
Robin van Leeuwen <rvanleeuwen@leukeleu.nl>
2026-06-02 09:50:48 +0200

    chore: add demo views

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 from ninja import NinjaAPI
 
+from piratespeak.views_mcp_ninja import router as mcp_router
 from piratespeak.views_memories_ninja import router as memories_router
 from piratespeak.views_ninja import router as piratespeak_router
 
@@ -27,10 +28,12 @@ api = NinjaAPI(title="Django AI SDK Demo", version="1.0.0")
 
 api.add_router("/piratespeak", piratespeak_router)
 api.add_router("/memories", memories_router)
+api.add_router("/mcp", mcp_router)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
     path("api/v2/", include("piratespeak.views_drf")),
     path("api/v2/", include("piratespeak.views_memories_drf")),
+    path("api/v2/", include("piratespeak.views_mcp_drf")),
 ]
