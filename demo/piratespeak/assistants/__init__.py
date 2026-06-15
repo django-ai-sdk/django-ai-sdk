e5ab746879491c86071d54318ff6d564fa97726e
terminalkitten <dk@amaniak.nl>
2026-06-15 03:56:59 +0200

    chore: export workspace assistant

--- demo/piratespeak/assistants/__init__.py
+++ demo/piratespeak/assistants/__init__.py
@@ -4,8 +4,10 @@ Pirate-themed assistants for the demo application.
 
 from .agent_swarm import AgentSwarmAssistant
 from .pirate_basic import PirateBasicAssistant
+from .workspace import WorkspaceAssistant
 
 __all__ = [
     "AgentSwarmAssistant",
     "PirateBasicAssistant",
+    "WorkspaceAssistant",
 ]
