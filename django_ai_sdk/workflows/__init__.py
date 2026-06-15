2f800b7b718e9b30a469bdd751d526c718c2f26b
terminalkitten <dk@amaniak.nl>
2026-06-15 22:18:26 +0200

    feat: workflow executor

---
+++ django_ai_sdk/workflows/__init__.py
@@ -0,0 +1,12 @@
+from .models import WorkflowSettings
+from .schemas import StepField, WorkflowAction, WorkflowDefinition, WorkflowStep
+from .services import WorkflowService
+
+__all__ = [
+    "StepField",
+    "WorkflowAction",
+    "WorkflowDefinition",
+    "WorkflowSettings",
+    "WorkflowStep",
+    "WorkflowService",
+]
