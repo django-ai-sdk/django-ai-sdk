6478241ba4cd3c09c9fd5875c17a804483ac2136
terminalkitten <dk@amaniak.nl>
2026-06-19 11:15:36 +0200

    chore: suppress stdout logger with ENV flag

--- django_ai_sdk/logger.py
+++ django_ai_sdk/logger.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from typing import TYPE_CHECKING
 
@@ -6,20 +7,16 @@ from loguru import logger
 if TYPE_CHECKING:
     from loguru import Logger
 
-
 logger.remove()
 
-# Add console handler
-logger.add(
-    sys.stderr,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
-    level="DEBUG",
-    colorize=True,
-)
+if not os.getenv("AI_SDK_QUIET"):
+    logger.add(
+        sys.stderr,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        level="DEBUG",
+        colorize=True,
+    )
 
-# Add file handler
-# I added this also for code agents, they can track the app,
-# without the need for a restart.
 logger.add(
     "logs/django_ai_sdk.log",
     format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name}:{function}:{line} - {message}",
